12677e29d3222195431d82bd74cf7b63036143b0
niolmo <71871013+niolmo@users.noreply.github.com>
2021-11-22 13:42:08 +0500

    Первая практика JS

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// Создаём объект
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {}, //пустой объект
+  actors: {},
+  genres: [],
+  privat: false
+
+};
+	
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+			b = prompt('На сколько оцените его?', ''),
+			c = prompt('Один из последних просмотренных фильмов?', ''),
+			d = prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
