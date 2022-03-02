37d2d47156b829d57e55406d57f48ebf9e2cbd2d
niolmo <71871013+niolmo@users.noreply.github.com>
2022-03-02 11:28:41 +0500

    v2

--- JS/script.js
+++ JS/script.js
@@ -13,13 +13,26 @@ const personalMovieDB = {
 
 };
 	
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-			b = prompt('На сколько оцените его?', ''),
-			c = prompt('Один из последних просмотренных фильмов?', ''),
-			d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+// 			b = prompt('На сколько оцените его?', ''),
+// 			c = prompt('Один из последних просмотренных фильмов?', ''),
+// 			d = prompt('На сколько оцените его?', '');
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+				b = prompt('На сколько оцените его?', '');
+		
+				if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+					personalMovieDB.movies[a] = b;
+					console.log('done');
+				} else {
+					console.log('error');
+					i--;
+				}
+}
 
 console.log(personalMovieDB);
