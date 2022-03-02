6a140fd7768b5a4d29cfbcbd7e60576f856e5470
niolmo <71871013+niolmo@users.noreply.github.com>
2022-03-02 11:40:15 +0500

    v3

--- JS/script.js
+++ JS/script.js
@@ -22,7 +22,7 @@ const personalMovieDB = {
 // personalMovieDB.movies[a] = b;
 // personalMovieDB.movies[c] = d;
 
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 1; i++) {
 	const a = prompt('Один из последних просмотренных фильмов?', ''),
 				b = prompt('На сколько оцените его?', '');
 		
@@ -32,6 +32,16 @@ for (let i = 0; i < 2; i++) {
 				} else {
 					console.log('error');
 					i--;
+				}
+
+				if(personalMovieDB.count < 10) {
+					console.log('Просмотрено давольно мало фильмов');
+				}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+					console.log('Вы классический зритель');
+				}else if(personalMovieDB.count >= 30) {
+					console.log('Вы киноман');
+				}else {
+					console.log('Произошла ошибка');
 				}
 }
 
